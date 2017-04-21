bc0c9135f2147e5abbd54393244f8746848052bf
Zy Mao <mzy12345@gmail.com>
2017-04-21 02:46:02 -0400

    Finish index page.

--- src/main/java/com/wpi/teamd/servlet/SearchServlet.java
+++ src/main/java/com/wpi/teamd/servlet/SearchServlet.java
@@ -20,7 +20,10 @@ public class SearchServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String departureAirportCode = request.getParameter("departure-airport");
 		String arrivalAirportCode = request.getParameter("arrival-airport");
-
+		String departureDate = request.getParameter("departure-date");
+		String arrivalDate = request.getParameter("arrival-date");
+		logger.debug(departureAirportCode);
+		logger.debug(departureDate);
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
